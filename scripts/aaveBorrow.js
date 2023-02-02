789759cc5200e1dc52af878f7be42739dd0ffe74
Fiat Bruno <fiatbruno10@gmail.com>
2023-02-02 07:32:30 +0200

    Added depositing function

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-process-exit */
-const { getWeth } = require("../scripts/getWeth")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
 const { ethers } = require("hardhat")
 
 async function main() {
@@ -7,13 +7,39 @@ async function main() {
     const accounts = await ethers.getSigners()
     const deployer = accounts[0]
 
-    
-    //Lending Pool Address Provider0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    //Lending Pool Addresses Provider = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
 
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+
+    // Deposit
+    const wethTokenAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    // Approve
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer.address, 0)
+    console.log("Deposited! ✨")
+
+    //Borrow
+    
 }
 
-async function getLendingPool(){
-    
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved! ✨")
 }
 
 main()
